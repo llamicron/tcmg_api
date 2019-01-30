0c7df4ea1f30414fb6c4733cd6ccd2b9830dd694
llamicron <llamicron@gmail.com>
2019-01-30 01:06:37 -0600

    Fixes tests to load json correctly

--- test_server.py
+++ test_server.py
@@ -12,28 +12,28 @@ def client():
 
 def test_md5(client):
     r = client.get('/md5/hash this please')
-    data = json.loads(r.data)
+    data = json.loads(str(r.data))
 
     assert data['output'] == 'f211faadbeacebe3ed95f3fc8116449a'
 
 def test_factorial(client):
     r = client.get('/factorial/7')
-    data = json.loads(r.data)
+    data = json.loads(str(r.data))
 
     assert data['output'] == 5040
 
     r = client.get('/factorial/-4')
-    data = json.loads(r.data)
+    data = json.loads(str(r.data))
 
     assert data['output'] == 'Error: integer needs to be positive'
 
 def test_fib(client):
     r = client.get('/fibonacci/20')
-    data = json.loads(r.data)
+    data = json.loads(str(r.data))
 
     assert data['output'] == [0, 1, 1, 2, 3, 5, 8, 13]
 
     r = client.get('/fibonacci/-4')
-    data = json.loads(r.data)
+    data = json.loads(str(r.data))
 
     assert data['output'] == 'Error: integer needs to be positive'
