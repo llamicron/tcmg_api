b2a7f827be3d14490e3244bd9462bc7abe12732a
llamicron <llamicron@gmail.com>
2019-01-30 01:17:20 -0600

    Removes some unneeded test code

--- test_server.py
+++ test_server.py
@@ -4,11 +4,6 @@ import pytest
 
 from server import app
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    yield app.test_client()
-
 def get(uri):
     app.config['TESTING'] = True
     client = app.test_client()
@@ -16,12 +11,12 @@ def get(uri):
     return json.loads(response.data.decode('utf-8'))
 
 
-def test_md5(client):
+def test_md5():
     data = get('/md5/hash this please')
 
     assert data['output'] == 'f211faadbeacebe3ed95f3fc8116449a'
 
-def test_factorial(client):
+def test_factorial():
     data = get('/factorial/7')
 
     assert data['output'] == 5040
@@ -30,7 +25,7 @@ def test_factorial(client):
 
     assert data['output'] == 'Error: integer needs to be positive'
 
-def test_fib(client):
+def test_fib():
     data = get('/fibonacci/20')
 
     assert data['output'] == [0, 1, 1, 2, 3, 5, 8, 13]
@@ -39,5 +34,3 @@ def test_fib(client):
 
     assert data['output'] == 'Error: integer needs to be positive'
 
-
-test_factorial(app.test_client())
