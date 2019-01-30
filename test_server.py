02e319799219b0c9c5b3dfc661528473fa53e0ed
llamicron <llamicron@gmail.com>
2019-01-30 01:16:42 -0600

    Fixes tests to use the right encoding

--- test_server.py
+++ test_server.py
@@ -2,38 +2,42 @@ import json
 
 import pytest
 
-from .server import app
+from server import app
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     yield app.test_client()
 
+def get(uri):
+    app.config['TESTING'] = True
+    client = app.test_client()
+    response = client.get(uri)
+    return json.loads(response.data.decode('utf-8'))
+
 
 def test_md5(client):
-    r = client.get('/md5/hash this please')
-    data = json.loads(str(r.data))
+    data = get('/md5/hash this please')
 
     assert data['output'] == 'f211faadbeacebe3ed95f3fc8116449a'
 
 def test_factorial(client):
-    r = client.get('/factorial/7')
-    data = json.loads(str(r.data))
+    data = get('/factorial/7')
 
     assert data['output'] == 5040
 
-    r = client.get('/factorial/-4')
-    data = json.loads(str(r.data))
+    data = get('/factorial/-4')
 
     assert data['output'] == 'Error: integer needs to be positive'
 
 def test_fib(client):
-    r = client.get('/fibonacci/20')
-    data = json.loads(str(r.data))
+    data = get('/fibonacci/20')
 
     assert data['output'] == [0, 1, 1, 2, 3, 5, 8, 13]
 
-    r = client.get('/fibonacci/-4')
-    data = json.loads(str(r.data))
+    data = get('/fibonacci/-4')
 
     assert data['output'] == 'Error: integer needs to be positive'
+
+
+test_factorial(app.test_client())
