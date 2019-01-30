25f47acf8b47509a9e9b1c613294962c0f9e9ac9
llamicron <llamicron@gmail.com>
2019-01-30 00:50:05 -0600

    Adds factorial route

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/md5/<string>')
 def md5(string):
     return json.dumps({
@@ -13,3 +12,18 @@ def md5(string):
         'output': hashlib.md5(bytes(string, 'utf-8')).hexdigest()
     })
 
+@app.route('/factorial/<number>')
+def factorial(number):
+    num = int(number)
+
+    if num < 0:
+        output = 'Error: integer needs to be positive'
+    else:
+        output = 1
+        for i in range(1, num + 1):
+            output = i * output
+
+    return json.dumps({
+        'input': number,
+        'output': output
+    })
--- test_server.py
+++ test_server.py
@@ -13,4 +13,16 @@ def client():
 def test_md5(client):
     r = client.get('/md5/hash this please')
     data = json.loads(r.data)
+
     assert data['output'] == 'f211faadbeacebe3ed95f3fc8116449a'
+
+def test_factorial(client):
+    r = client.get('/factorial/7')
+    data = json.loads(r.data)
+
+    assert data['output'] == 5040
+
+    r = client.get('/factorial/-4')
+    data = json.loads(r.data)
+
+    assert data['output'] == 'Error: integer needs to be positive'
